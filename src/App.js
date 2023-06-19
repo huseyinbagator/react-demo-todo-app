65abfa04c09c03422c1beaf3a9e266d7c722fe15
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-06-19 18:17:07 +0300

    todo-design-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>To Do List</h1>
+        <form className="form">
+          {/* Serach Bar Start */}
+          <div>
+            <input  placeholder="New To-do"></input>
+          </div>
+          {/* Serach Bar End */}
+         
+         {/* Buttons Section Start */}
+          <div className="buttons">
+            <button className="buttonadd" type="submit">Add</button>
+            <button className="buttonadelete" type="submit">Delete</button>
+          </div>
+          {/* Buttons Section End */}
+        </form>
+
+          {/* To Do  Section Start */}
+        <div className="todolist">
+        
+        </div>
+         {/* To Do  Section End */}
+
     </div>
   );
 }
