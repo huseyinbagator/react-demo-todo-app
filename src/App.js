4034ca5bf066ad7b0061c1678dc892f141c9da33
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-06-20 18:23:11 +0300

    todo-design-3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
 
 import './App.css';
 import {useState} from 'react';
+
 function App() {
   const [value, setvalue] = useState(" ");
+  const [savedValue, setsavedValue] = useState([]);
   const handleChange = (e) => {
   setvalue(e.target.value)
   console.log(e.target.value)
-}
+  }
+  const handleadd = (event) =>{
+    setsavedValue([...savedValue,value]);
+    setvalue('');
+    event.preventDefault();
+  }
   return (
    <>
-      
-   <div className="App">
-        <h1>To Do List</h1>
-        <form className="form" >
-          {/* Serach Bar Start */}
-          <div>
-            <input value={value}  onChange={handleChange} placeholder="New To-do"></input>
-          </div>
-          {/* Serach Bar End */}
-         
-         {/* Buttons Section Start */}
-          <div className="buttons">
-            <button className="buttonadd" type="submit">Add</button>
-            <button className="buttonadelete" type="submit">Delete</button>
-          </div>
+      <div className="App">
+         <h1>To Do List</h1>
+          <form className="form" >
+           {/* Serach Bar Start */}
+            <div>
+                <input value={value}  onChange={handleChange} placeholder="New To-do">
+                </input>
+            </div>
+           {/* Serach Bar End */}
+            
+          {/* Buttons Section Start */}
+            <div className="buttons">
+              <button className="buttonadd" onClick={handleadd} type="submit">Add</button>
+               <button className="buttonadelete" type="submit">Delete</button>
+            </div>
           {/* Buttons Section End */}
-        </form>
+          </form>
 
           {/* To Do  Section Start */}
-        <div className="todolist">
-        
-        </div>
-         {/* To Do  Section End */}
+            <div className="todolist">
+                <ul>
+                  {savedValue.map((value,index) =>(
+                      <li key={index}>{value}</li>
+                  ))}
+                  
+                </ul>
+            </div>
+          {/* To Do  Section End */}
 
-    </div>
+       </div>
     </>
   );
 }
