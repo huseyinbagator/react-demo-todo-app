abc39391b2641f13239bf2721af711635c08295f
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-08-07 11:07:37 +0300

    project-prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import TasksPage from './pages/TasksPage';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import TasksPage from "./pages/TasksPage";
+import "./App.css";
 function App() {
-
   return (
     <BrowserRouter>
       <div>
-          <Routes>
-            <Route path="/"  element={<LoginPage/>} />
-            <Route path='/tasks' element={<TasksPage/>} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/tasks" element={<TasksPage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,67 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import image from '../images/image-1.png';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import image from "../images/image-1.png";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-    const [Username, setUsername] = useState('');
-    const [Password, setPassword] = useState('');
-    const navigate = useNavigate();
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleUsernameChange = (event) => {
-        setUsername(event.target.value);
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = (event) => {
+    if (Username === "admin" && Password === "admin") {
+      localStorage.setItem("username", "admin");
+      navigate("/tasks");
+    } else {
+      alert("Username or password is wrong");
     }
+    event.preventDefault();
+  };
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
+  useEffect(() => {
+    const loginValidation = localStorage.getItem("username");
+    if (loginValidation) {
+      navigate("/tasks");
     }
+  }, [navigate]);
 
-    const handleLogin = (event) => {
-        if (Username === "admin" && Password === 'admin') {
-            localStorage.setItem('username', 'admin');
-            navigate('/tasks');
-        } else {
-            alert('Username or password is wrong');
-        }
-        event.preventDefault();
-    }
+  return (
+    <div className="LoginPage">
+      <div className="container">
+        <div className="right">
+          <h2 className="h2title">Welcome to To Do!</h2>
+          <p className="ptitle">
+            "Manage your time, organize your tasks with To Do!"
+          </p>
+          <h1 className="h1title">Log In</h1>
 
-    useEffect(() => {
-        const loginValidation = localStorage.getItem('username');
-        if (loginValidation) {
-            navigate('/tasks');
-        }
-    }, [navigate]);
-
-    return (
-        <div className='LoginPage'>
-            <div className='container'>
-                <div className='right'>
-                    <h2 className='h2title'>Welcome to To Do!</h2>
-                    <p className='ptitle'>"Manage your time, organize your tasks with To Do!"</p>
-                    <h1 className='h1title'>Log In</h1>
-
-                    <form className='formLogin' onSubmit={handleLogin}>
-                        <input
-                            type='text'
-                            className='inpt'
-                            onChange={handleUsernameChange}
-                        />
-                        <input
-                            className='inpt'
-                            type='password'
-                            onChange={handlePasswordChange}
-                        />
-                        <div className='buttonLog'>
-                            <button className='btn log'>Login</button>
-                        </div>
-                    </form>
-                </div>
-                <div className='left'>
-                    <img className='image' alt='Image' src={image} />
-                </div>
+          <form className="formLogin" onSubmit={handleLogin}>
+            <input
+              type="text"
+              className="inpt"
+              onChange={handleUsernameChange}
+            />
+            <input
+              className="inpt"
+              type="password"
+              onChange={handlePasswordChange}
+            />
+            <div className="buttonLog">
+              <button className="btn log">Login</button>
             </div>
+          </form>
         </div>
-    );
+        <div className="left">
+          <img className="image" alt="Image" src={image} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
